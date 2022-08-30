b4557245f4d5d3f34f1e99a91b088c50a33aef19
Michael J. Lee <leem@starktechgroup.com>
2022-08-30 19:52:35 -0400

    add event-rule

--- pkg/domain/conn.go
+++ pkg/domain/conn.go
@@ -1,15 +1,15 @@
 package domain
 
 type Conns struct {
-	Count	int32 	`json:"count"`
-	Conns 	[]*Conn	`json:"conns"`
+	Count int32   `json:"count"`
+	Conns []*Conn `json:"conns"`
 }
 
 type Conn struct {
-	Id   	int    	`json:"id,omitempty"`
-	Ref 	string 	`json:"ref,omitempty"`
-	Enabled	bool 	`json:"enabled"`
-	Name 	string	`json:"name,omitempty"`
-	Type 	string 	`json:"type"`
-	Audit	*Audit	`json:"audit"`
+	Id      int    `json:"id,omitempty"`
+	Ref     string `json:"ref,omitempty"`
+	Enabled bool   `json:"enabled"`
+	Name    string `json:"name,omitempty"`
+	Type    string `json:"type"`
+	Audit   *Audit `json:"audit,omitempty"`
 }

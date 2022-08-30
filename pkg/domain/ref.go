1b704a59031aa3b74e5da77214e45185d16a8725
Michael Sweeney <Sweeneym@starktechgroup.com>
2022-08-30 15:27:57 -0400

    Updated WIP

--- pkg/domain/ref.go
+++ pkg/domain/ref.go
@@ -1,6 +1,8 @@
 package domain
 
 import (
+	"fmt"
+	"math/rand"
 	"regexp"
 	"strings"
 )
@@ -16,11 +18,11 @@ const (
 	BranchTable   = "asset_tree_branch"
 	TagRefType    = "g"
 	TagRefTable   = "tag_ref"
-	ALPHABET = "abcdefghijklmnopqrstuvwxyz0123456789"
-	DEFAULT_LENGHT = 16
-	DEFAULT_SPACEING = 8
-	DEFAULT_SPACEING_CHAR = '-'
-	DEFAULT_PREFIX_CHAR = '.'
+	Alphabet = "abcdefghijklmnopqrstuvwxyz0123456789"
+	DefaultLength = 16
+	DefaultSpacing = 8
+	DefaultSpacingChar = "-"
+	DefaultPrefixChar = "."
 )
 
 var pattern = regexp.MustCompile(`^([a-z]{1,8}\.[a-z0-9]{8}-[a-z0-9]{8}([:]\d+)?)$`)
@@ -51,6 +53,33 @@ func (r *Ref) GetPrefix() string {
 //String returns the value
 func (r Ref) String() string {
 	return r.Value
+}
+
+func randomChar() string{
+	return string(Alphabet[rand.Intn(len(Alphabet))])
+}
+
+func randomWithPrefix(prefix string) string{
+	return fmt.Sprintf("%s%s%s", prefix, DefaultPrefixChar, random(DefaultLength, DefaultSpacing, DefaultSpacingChar))
+}
+
+func randomWithoutPrefix() string{
+	return random(DefaultLength, DefaultSpacing, DefaultSpacingChar)
+}
+
+func random(length int, spacing int, spacerChar string) string{
+	rndString := ""
+	spacer := 0
+	for length > 0 {
+		if spacer == spacing {
+			rndString += spacerChar
+			spacer = 0
+		}
+	length--
+	spacer++
+		rndString += randomChar()
+	}
+	return rndString
 }
 
 //CreateRef creates and returns new ref for an asset

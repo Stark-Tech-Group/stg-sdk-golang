e2bcc5827ea4ab84c5612ae43c530a2cfbc1d728
Michael J. Lee <leem@starktechgroup.com>
2022-08-30 19:42:46 -0400

    omit if empty

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/Stark-Tech-Group/stg-sdk-golang
 
-go 1.15
+go 1.17
 
 require (
 	github.com/Azure/azure-event-hubs-go/v3 v3.3.18
--- pkg/domain/site.go
+++ pkg/domain/site.go
@@ -1,25 +1,25 @@
 package domain
 
 type Sites struct {
-	Count	int32 	`json:"count"`
-	Sites 	[]*Site `json:"sites"`
+	Count int32   `json:"count"`
+	Sites []*Site `json:"sites"`
 }
 
-type Site struct{
-	Name 			string 		`json:"name,omitempty"`
-	Ref 			string 		`json:"ref,omitempty"`
-	Id 				int32 		`json:"id,omitempty"`
-	Equips 			[]Equip 	`json:"equips"`
-	Profile			Profile 	`json:"profile"`
-	Latitude  		float64 	`json:"latitude,omitempty"`
-	Longitude 		float64 	`json:"longitude,omitempty"`
-	Enabled 		bool 		`json:"enabled"`
-	Description 	string 		`json:"description,omitempty"`
-	GeoCity 		string 		`json:"geoCity,omitempty"`
-	GeoStateCode 	string 		`json:"geoStateCode,omitempty"`
-	GeoAddress1 	string 		`json:"geoAddress1,omitempty"`
-	GeoAddress2 	string 		`json:"geoAddress2,omitempty"`
-	GeoPostalCode 	string 		`json:"geoPostalCode,omitempty"`
-	Audit 			Audit		`json:"audit"`
-	Conn			Conn		`json:"conn"`
+type Site struct {
+	Name          string   `json:"name,omitempty"`
+	Ref           string   `json:"ref,omitempty"`
+	Id            int32    `json:"id,omitempty"`
+	Equips        *[]Equip `json:"equips,omitempty"`
+	Profile       *Profile `json:"profile,omitempty"`
+	Latitude      float64  `json:"latitude,omitempty"`
+	Longitude     float64  `json:"longitude,omitempty"`
+	Enabled       bool     `json:"enabled,omitempty"`
+	Description   string   `json:"description,omitempty"`
+	GeoCity       string   `json:"geoCity,omitempty"`
+	GeoStateCode  string   `json:"geoStateCode,omitempty"`
+	GeoAddress1   string   `json:"geoAddress1,omitempty"`
+	GeoAddress2   string   `json:"geoAddress2,omitempty"`
+	GeoPostalCode string   `json:"geoPostalCode,omitempty"`
+	Audit         *Audit   `json:"audit,omitempty"`
+	Conn          *Conn    `json:"conn,omitempty"`
 }

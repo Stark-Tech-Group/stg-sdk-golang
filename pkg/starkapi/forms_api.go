92c87c489a4b76c0d0d4a16868468c8a2d996f13
Michael Sweeney <Sweeneym@starktechgroup.com>
2022-09-09 14:48:59 -0400

    Added new form control domain object and get methods for get all form controls, and get all form fields by ref for an asset. WIP

---
+++ pkg/starkapi/forms_api.go
@@ -0,0 +1,59 @@
+package starkapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/Stark-Tech-Group/stg-sdk-golang/pkg/domain"
+)
+
+type FormsApi struct {
+	client *Client
+}
+
+func (formsApi *FormsApi) host() string {
+	return formsApi.client.host
+}
+
+func (formsApi *FormsApi) BaseUrl() string {
+	return fmt.Sprintf("%s/core/forms", formsApi.host())
+}
+
+func (formsApi *FormsApi) ControlsPrefix() string {
+	return fmt.Sprintf("/controls")
+}
+
+func (formsApi *FormsApi) GetAllControls() (domain.FormControlList, error) {
+	url := fmt.Sprintf("%s%s", formsApi.BaseUrl(), formsApi.ControlsPrefix())
+
+	var controls domain.FormControlList
+
+	resp, err := formsApi.client.get(url)
+	if err != nil {
+		return controls, err
+	}
+
+	err = json.Unmarshal(resp, &controls)
+	if err != nil {
+		return controls, err
+	}
+
+	return controls, nil
+}
+
+func (formsApi *FormsApi) GetAllControlsForAsset(ref string) (domain.FormControlList, error) {
+	url := fmt.Sprintf("%s/%s%s", formsApi.BaseUrl(), ref, formsApi.ControlsPrefix())
+
+	var assetControls domain.FormControlList
+
+	resp, err := formsApi.client.get(url)
+	if err != nil {
+		return controls, err
+	}
+
+	err = json.Unmarshal(resp, &controls)
+	if err != nil {
+		return controls, err
+	}
+
+	return controls, nil
+}

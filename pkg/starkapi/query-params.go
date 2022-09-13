92821f5afcc553da74b964b2e1725ea5ea2821c5
Michael J. Lee <leem@starktechgroup.com>
2022-09-13 07:58:54 -0400

    add request to query params

--- pkg/starkapi/query-params.go
+++ pkg/starkapi/query-params.go
@@ -30,26 +30,28 @@ var operatorMap = map[string]string{
 var input = regexp.MustCompile("^([a-z]|[A-Z]|[0-9]|[.]|-){1,75}$")
 
 type QueryParams struct {
-	Id       string `json:"id" schema:"id" sqlColumn:"id" sqlType:"bigint"`
-	Ref      string `json:"ref" schema:"ref" sqlColumn:"ref" sqlType:"text"`
-	SiteId   string `json:"siteId" schema:"siteId" sqlColumn:"site_id" sqlType:"bigint"`
-	SiteRef  string `json:"siteRef" schema:"siteRef" sqlColumn:"site_ref" sqlType:"text"`
-	EquipId  string `json:"equipId" schema:"equipId" sqlColumn:"equip_id" sqlType:"bigint"`
-	EquipRef string `json:"equipRef" schema:"equipRef" sqlColumn:"equip_ref" sqlType:"text"`
-	RuleName string `json:"ruleName" schema:"ruleName" sqlColumn:"rule_name" sqlType:"text"`
-	RuleId   string `json:"ruleId" schema:"ruleId" sqlColumn:"rule_id" sqlType:"bigint"`
-	Severity string `json:"severity" schema:"severity" sqlColumn:"severity" sqlType:"int"`
-	Duration string `json:"dur" schema:"dur" sqlColumn:"dur" sqlType:"bigint"`
-	PersonId string `json:"personId" schema:"personId" sqlColumn:"person_id" sqlType:"bigint"`
-	Ts       string `json:"ts" schema:"ts" sqlColumn:"ts" sqlType:"bigint"`
-	EndTs    string `json:"endTs" schema:"endTs" sqlColumn:"end_ts" sqlType:"bigint"`
-	Limit    int    `json:"limit" schema:"limit"`
-	Offset   int    `json:"offset" schema:"offset"`
+	Id          string `json:"id" schema:"id" sqlColumn:"id" sqlType:"bigint"`
+	Ref         string `json:"ref" schema:"ref" sqlColumn:"ref" sqlType:"text"`
+	SiteId      string `json:"siteId" schema:"siteId" sqlColumn:"site_id" sqlType:"bigint"`
+	SiteRef     string `json:"siteRef" schema:"siteRef" sqlColumn:"site_ref" sqlType:"text"`
+	EquipId     string `json:"equipId" schema:"equipId" sqlColumn:"equip_id" sqlType:"bigint"`
+	EquipRef    string `json:"equipRef" schema:"equipRef" sqlColumn:"equip_ref" sqlType:"text"`
+	RuleName    string `json:"ruleName" schema:"ruleName" sqlColumn:"rule_name" sqlType:"text"`
+	RuleId      string `json:"ruleId" schema:"ruleId" sqlColumn:"rule_id" sqlType:"bigint"`
+	Severity    string `json:"severity" schema:"severity" sqlColumn:"severity" sqlType:"int"`
+	Duration    string `json:"dur" schema:"dur" sqlColumn:"dur" sqlType:"bigint"`
+	PersonId    string `json:"personId" schema:"personId" sqlColumn:"person_id" sqlType:"bigint"`
+	Ts          string `json:"ts" schema:"ts" sqlColumn:"ts" sqlType:"bigint"`
+	EndTs       string `json:"endTs" schema:"endTs" sqlColumn:"end_ts" sqlType:"bigint"`
+	Limit       int    `json:"limit" schema:"limit"`
+	Offset      int    `json:"offset" schema:"offset"`
+	RequestName string `json:"-" schema:"-"`
 }
 
 //HashKey creates a compounded string of the current QueryParams
 func (q *QueryParams) HashKey() string {
-	return fmt.Sprintf("%s-%s-%s-%s-%s-%s-%s-%s-%s-%s-%s-%s-%v-%v",
+	return fmt.Sprintf("%s-%s-%s-%s-%s-%s-%s-%s-%s-%s-%s-%s-%s-%v-%v",
+		q.RequestName,
 		q.Id,
 		q.Ref,
 		q.SiteId,
--- pkg/starkapi/query-params_test.go
+++ pkg/starkapi/query-params_test.go
@@ -7,35 +7,39 @@ import (
 
 func TestQueryParams_HashKey(t *testing.T) {
 	p := QueryParams{}
-	assert.Equal(t, "------------0-0", p.HashKey())
+	assert.Equal(t, "-------------0-0", p.HashKey())
+
+	assert.Equal(t, "-------------0-0", p.HashKey())
+	p.RequestName = "R"
+	assert.Equal(t, "R-------------0-0", p.HashKey())
 	p.Id = "1"
-	assert.Equal(t, "1------------0-0", p.HashKey())
+	assert.Equal(t, "R-1------------0-0", p.HashKey())
 	p.Ref = "2"
-	assert.Equal(t, "1-2-----------0-0", p.HashKey())
+	assert.Equal(t, "R-1-2-----------0-0", p.HashKey())
 	p.SiteId = "3"
-	assert.Equal(t, "1-2-3----------0-0", p.HashKey())
+	assert.Equal(t, "R-1-2-3----------0-0", p.HashKey())
 	p.SiteRef = "4"
-	assert.Equal(t, "1-2-3-4---------0-0", p.HashKey())
+	assert.Equal(t, "R-1-2-3-4---------0-0", p.HashKey())
 	p.EquipRef = "5"
-	assert.Equal(t, "1-2-3-4-5--------0-0", p.HashKey())
+	assert.Equal(t, "R-1-2-3-4-5--------0-0", p.HashKey())
 	p.RuleName = "6"
-	assert.Equal(t, "1-2-3-4-5-6-------0-0", p.HashKey())
+	assert.Equal(t, "R-1-2-3-4-5-6-------0-0", p.HashKey())
 	p.RuleId = "7"
-	assert.Equal(t, "1-2-3-4-5-6-7------0-0", p.HashKey())
+	assert.Equal(t, "R-1-2-3-4-5-6-7------0-0", p.HashKey())
 	p.Severity = "8"
-	assert.Equal(t, "1-2-3-4-5-6-7-8-----0-0", p.HashKey())
+	assert.Equal(t, "R-1-2-3-4-5-6-7-8-----0-0", p.HashKey())
 	p.Duration = "9"
-	assert.Equal(t, "1-2-3-4-5-6-7-8-9----0-0", p.HashKey())
+	assert.Equal(t, "R-1-2-3-4-5-6-7-8-9----0-0", p.HashKey())
 	p.PersonId = "10"
-	assert.Equal(t, "1-2-3-4-5-6-7-8-9-10---0-0", p.HashKey())
+	assert.Equal(t, "R-1-2-3-4-5-6-7-8-9-10---0-0", p.HashKey())
 	p.Ts = "11"
-	assert.Equal(t, "1-2-3-4-5-6-7-8-9-10-11--0-0", p.HashKey())
+	assert.Equal(t, "R-1-2-3-4-5-6-7-8-9-10-11--0-0", p.HashKey())
 	p.EndTs = "12"
-	assert.Equal(t, "1-2-3-4-5-6-7-8-9-10-11-12-0-0", p.HashKey())
+	assert.Equal(t, "R-1-2-3-4-5-6-7-8-9-10-11-12-0-0", p.HashKey())
 	p.Limit = 13
-	assert.Equal(t, "1-2-3-4-5-6-7-8-9-10-11-12-13-0", p.HashKey())
+	assert.Equal(t, "R-1-2-3-4-5-6-7-8-9-10-11-12-13-0", p.HashKey())
 	p.Offset = 14
-	assert.Equal(t, "1-2-3-4-5-6-7-8-9-10-11-12-13-14", p.HashKey())
+	assert.Equal(t, "R-1-2-3-4-5-6-7-8-9-10-11-12-13-14", p.HashKey())
 }
 func TestQueryParams_DecodeParameters_WithDefaultOperator(t *testing.T) {
 	p := QueryParams{Id: "1"}

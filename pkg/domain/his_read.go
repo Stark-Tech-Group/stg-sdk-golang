ed8cec794b455299dfe5fe5d7d11aa5e839feb4f
Stewart Shear <shears@starktech.com>
2023-09-15 15:17:15 -0400

    moved field outside of point

--- pkg/domain/his_read.go
+++ pkg/domain/his_read.go
@@ -2,19 +2,19 @@ package domain
 
 type HisRead struct {
 	Point struct {
-		ID         int    `json:"id"`
-		Name       string `json:"name"`
-		Type       string `json:"type"`
-		Unit       string `json:"unit"`
-		DisplayVal string `json:"displayValue"`
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+		Type string `json:"type"`
+		Unit string `json:"unit"`
 	} `json:"member"`
-	Start  int    `json:"start"`
-	End    int    `json:"end"`
-	Size   int    `json:"size"`
-	Limit  int    `json:"limit"`
-	Offset int    `json:"offset"`
-	Source string `json:"source"`
-	His    []struct {
+	Start      int    `json:"start"`
+	End        int    `json:"end"`
+	Size       int    `json:"size"`
+	Limit      int    `json:"limit"`
+	Offset     int    `json:"offset"`
+	Source     string `json:"source"`
+	DisplayVal string `json:"displayValue"`
+	His        []struct {
 		Urid string  `json:"urid"`
 		Ts   int     `json:"ts"`
 		Val  float64 `json:"val"`
